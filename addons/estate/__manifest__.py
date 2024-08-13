efc667cb1477fdf6969f9093fc00a6d352f6141d
Dennis2340 <kamaradennis36@gmail.com>
2024-08-13 04:02:07 +0000

    added a manifest, models, and also some data field in the model and i am currently working on views

---
+++ addons/estate/__manifest__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'Real Estate',
+    'version': '1.0',
+    'category': '',
+    'sequence': 15,
+    'summary': 'real estate',
+    'description': "",
+    'website': '',
+    'depends': [
+        'base_setup',
+    ],
+    'data': [
+        'views/estate_property_views.xml'
+    ],
+    'demo': [
+      
+    ],
+    
+    'installable': True,
+    'application': True,
+    'auto_install': False
+}
